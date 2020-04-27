e1d5c71febbfd3243d13a920993beb695217a1f4
Pradeep <pradeep.s@transneuron.com>
2020-04-27 15:09:42 +0530

    initial commit for angular

---
+++ Angular6/src/app/app.module.ts
@@ -0,0 +1,28 @@
+/* Built-in imports */
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+/* Component imports */
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    SignUpComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
