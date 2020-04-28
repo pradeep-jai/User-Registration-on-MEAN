2f501a49b7c9356bce963fd3f5a5d42d7118c11e
Pradeep <pradeep.s@transneuron.com>
2020-04-28 18:22:11 +0530

    Authenticatio in Node js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const passport = require('passport');
+const _ = require('lodash');
 
 const User = mongoose.model('User');
 
@@ -17,4 +19,30 @@ module.exports.register = (req, res, next) => {
                 return next(err);
         }
     });
+};
+
+module.exports.authenticate = (req, res, next) => {
+    // call for passport authentication
+    passport.authenticate('local', (err, user, info) => {
+        // error from passport middleware
+        if (err) 
+            return res.status(400).json(err);
+        else if (user) 
+        // registered user
+            return res.status(200).json({ "token": user.generateJwt() });
+        else
+        // unknown user or wrong password
+            return res.status(404).json(info);
+    })(req, res);
+};
+
+module.exports.userProfile = (req, res, next) =>{
+    User.findOne({ _id: req._id },
+        (err, user) => {
+            if (!user)
+                return res.status(404).json({ status: false, message: 'User record not found.' });
+            else
+                return res.status(200).json({ status: true, user : _.pick(user,['fullName','email']) });
+        }
+    );
 }
--- server/routes/index.router.js
+++ server/routes/index.router.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 const ctrlUser = require('../controllers/user.controller');
 
+const jwtHelper = require('../config/jwtHelper');
+
 router.post('/register', ctrlUser.register);
+router.post('/authenticate', ctrlUser.authenticate);
+router.get('/userProfile',jwtHelper.verifyJwtToken, ctrlUser.userProfile);
 
 module.exports = router;
 
