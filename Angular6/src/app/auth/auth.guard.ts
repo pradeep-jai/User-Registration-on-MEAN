56b4669be17b312fad27290eb7289977f47ef0d3
Pradeep <pradeep.s@transneuron.com>
2020-04-30 17:13:35 +0530

    Alignment changed.

--- Angular6/src/app/auth/auth.guard.ts
+++ Angular6/src/app/auth/auth.guard.ts
@@ -9,14 +9,12 @@ import { Router } from "@angular/router";
 })
 export class AuthGuard implements CanActivate {
   constructor(private userService : UserService, private router : Router) {}
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): boolean {
-      if (!this.userService.isLoggedIn()) {
-        this.router.navigateByUrl('/login');
-        this.userService.deleteToken();
-        return false;
-      }
+  canActivate(next: ActivatedRouteSnapshot,state: RouterStateSnapshot): boolean {
+    if (!this.userService.isLoggedIn()) {
+      this.router.navigateByUrl('/login');
+      this.userService.deleteToken();
+      return false;
+    }
     return true;
   }
   
